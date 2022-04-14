34f8b478740bfe866d6d2d5b6ea750d9c49940bd
Иван Алексеев <seevvia@gmail.com>
2022-04-14 12:32:45 +0300

    вторые изменения

--- test_bot.py
+++ test_bot.py
@@ -1,3 +1,4 @@
+"""Использования API для переотправки сообщений."""
 import os
 import time
 
@@ -13,13 +14,15 @@ TOKEN = os.getenv('TOKEN')  # Получаем токен Telegram.
 TIME_TO_WAIT = 60  # Время ожидания между запросами.
 
 vk_session = vk_api.VkApi(token=TOKEN)  # Авторизируем сессию в ВК.
-longpoll = VkLongPoll(vk_session)  # Подключаем Long Polling для получения сообщений.
+longpoll = VkLongPoll(vk_session)  # Подключаем Long Polling
 chat_id_for_error = 957044130  # Чат ID автора, на случай ошибки.
 chat_id = 957044130  # целевой Чат ID для отправки собщений.
+
 
 def send_message(bot, message):
     """Функция отправки сообщений."""
     bot.send_message(chat_id, message)
+
 
 def check_tokens():
     """Проверка получения токенов."""
@@ -28,28 +31,33 @@ def check_tokens():
     else:
         return True
 
+
 def main():
     """Основная функция запуска.
-       longpoll.listen() - ожидаем обновлений с сервера.
-       Если пришло нове сообщение, то пересылаем
-       его в телеграмм с указанным chat_id"""
+
+    longpoll.listen() - ожидаем обновлений с сервера.
+    Если пришло нове сообщение, то пересылаем
+    его в телеграмм с указанным chat_id
+    """
     check_tokens()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     count = 0
     while True:
         try:
             for event in longpoll.listen():
-                if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+                if (event.type == VkEventType.MESSAGE_NEW
+                        and event.to_me and event.text):
                     if event.from_user:
-                        msg = (f'В ВК пришло новое сообщение!: "{event.message}" '
+                        msg = (f'В ВК пришло новое сообщение!: '
+                               f'"{event.message}" '
                                f'от пользователя: {event.user_id} ')
-                        send_message(bot, msg)       
+                        send_message(bot, msg)
                         time.sleep(TIME_TO_WAIT)
         except Exception as error:
             if count < 1:
                 bot.send_message(chat_id_for_error, f"Ошибка: {error}")
                 count += 1
 
+
 if __name__ == '__main__':
     main()
-
